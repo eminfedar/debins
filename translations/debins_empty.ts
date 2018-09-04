b334e8a4b1258c7e359e7641d093e5ca143eae4f
Emin Fedar <eminfedar@gmail.com>
2018-09-04 23:00:47 +0300

    Added Turkish Translation

---
+++ translations/debins_empty.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainForm.ui</name>
+    <message>
+        <location filename="../MainForm.ui.qml" line="99"/>
+        <location filename="../MainForm.ui.qml" line="230"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="154"/>
+        <source>Uninstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="194"/>
+        <source>Installing...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="206"/>
+        <source>Uninstalling...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="220"/>
+        <source>Upgrade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="223"/>
+        <source>Downgrade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="226"/>
+        <source>Reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ddpkg.h" line="254"/>
+        <source>We have some issues while installing the package.
+Please run from terminal to see the errors.
+
+Command: debins file.deb</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="256"/>
+        <source>We couldn&apos;t find the file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="258"/>
+        <source>A Program using the package manager (dpkg) now. Please stop it first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="260"/>
+        <source>Please enter your root password to make Debins install the package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="35"/>
+        <source>Successfully Installed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="41"/>
+        <source>Reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <location filename="../main.qml" line="68"/>
+        <source>Error: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="56"/>
+        <source>Successfully Uninstalled!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/debins_tr.ts
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>MainForm.ui</name>
+    <message>
+        <location filename="../MainForm.ui.qml" line="99"/>
+        <location filename="../MainForm.ui.qml" line="230"/>
+        <source>Install</source>
+        <translation>Kur</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="154"/>
+        <source>Uninstall</source>
+        <translation>Kaldır</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="194"/>
+        <source>Installing...</source>
+        <translation>Kuruluyor...</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="206"/>
+        <source>Uninstalling...</source>
+        <translation>Kaldırılıyor...</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="220"/>
+        <source>Upgrade</source>
+        <translation>Yükselt</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="223"/>
+        <source>Downgrade</source>
+        <translation>Sürüm Düşür</translation>
+    </message>
+    <message>
+        <location filename="../MainForm.ui.qml" line="226"/>
+        <source>Reinstall</source>
+        <translation>Tekrar Kur</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../ddpkg.h" line="254"/>
+        <source>We have some issues while installing the package.
+Please run from terminal to see the errors.
+
+Command: debins file.deb</source>
+        <translation>Paketi kurarken bazı sorunlar yaşıyoruz.
+Lütfen sorunları görmek için programı uçbirimden açın.
+
+Komut: debins dosya.deb</translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="256"/>
+        <source>We couldn&apos;t find the file.</source>
+        <translation>Dosyayı bulamadık.</translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="258"/>
+        <source>A Program using the package manager (dpkg) now. Please stop it first.</source>
+        <translation>Bir program paket yöneticisini (dpkg) kullanıyor. Lütfen önce onu durdurun.</translation>
+    </message>
+    <message>
+        <location filename="../ddpkg.h" line="260"/>
+        <source>Please enter your root password to make Debins install the package.</source>
+        <translation>Lütfen Debins&apos;in paketi kurabilmesi için root şifrenizi girin.</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="35"/>
+        <source>Successfully Installed!</source>
+        <translation>Başarıyla Kuruldu!</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="41"/>
+        <source>Reinstall</source>
+        <translation>Tekrar Kur</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="49"/>
+        <location filename="../main.qml" line="68"/>
+        <source>Error: </source>
+        <translation>Hata: </translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="56"/>
+        <source>Successfully Uninstalled!</source>
+        <translation>Başarıyla Kaldırıldı!</translation>
+    </message>
+</context>
+</TS>
